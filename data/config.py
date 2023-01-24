7f9116adea65bf4d788b9c3cb259f405d67d7092
Daniil <51797415+lildaniil@users.noreply.github.com>
2023-01-24 19:15:55 +0300

    add db and stats

--- data/config.py
+++ data/config.py
@@ -1,10 +1,16 @@
 from environs import Env
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
 env.read_env()
 
+# Main settings
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
+# DB
+PGUSER=env.str("PGUSER")
+PGPASSWORD=env.str("PGPASSWORD")
+DATABASE=env.str("DATABASE")
+
+POSTGRES_URI = f'postgresql://{PGUSER}:{PGPASSWORD}@{IP}/{DATABASE}'
